d253e31cf564b028de81da198857b210af75e703
Sowmya7842 <112716943+Sowmya7842@users.noreply.github.com>
2022-09-21 00:09:13 +0530

    Delete two  sorted  lists.py

--- two  sorted  lists.py
+++
@@ -1,6 +0,0 @@
-test_list1=[]
-test_list2=[1,2,3,4,5]
-print("the list 1 is:"+str(test_list1))
-print("the list 2 is:"+str(test_list2))
-res=sorted(test_list1+test_list2)
-print("the combined sorted list is :"+str(res))
